0a01fa38e04216835df69ff78cec1651d8cf298a
lishuna <freefish0408@gmail.com>
2019-05-23 23:21:34 +0800

    Vue项目

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+module.export = {
+  publicPath: './',
+  outputDir: 'dist',
+  //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
+  assetsDir: 'assets',
+  configureWebpack: function (config) {
+    if (process.env.NODE_ENV === 'production') {
+      // 为生产环境修改配置...
+    } else {
+      // 为开发环境修改配置...
+    }
+  },
+  devServer:{
+    host: '0.0.0.0',
+    port: 3002,
+    hot: true,
+    before: function(app, server) {
+      app.post('*.json', function(req, res) {
+        res.json(fs.readFileSync(path.resolve(__dirname,'/mock/',req.url)));
+      });
+    }
+  }
+};
